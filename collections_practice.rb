970fc7e0159dd873fa8f3de8f436a4748d952607
Mohammed Noori <kmanoorib@gmail.com>
2018-05-26 03:50:23 +0000

    automatically backed up by learn

--- collections_practice.rb
+++ collections_practice.rb
@@ -10,3 +10,11 @@ end
 def sort_array_char_count(array)
   array.sort{|x, y| x.length <=> y.length}
 end
+
+def swap_elements(array)
+  hold_i2 = array[1]
+  hold_i3 = array[2]
+  array[1] = hold_i3
+  array[2] = hold_i2
+  array
+end
